998a10becf56ed44b813db9475134a5e4671a26a
theaaronmartin <amartin@ultrasignup.com>
2025-04-28 12:37:48 -0700

    removing multi-purpose thumb buttons

--- keyboards/beekeeb/piantor/keymaps/theaaronmartin/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/theaaronmartin/keymap.c
@@ -38,15 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
     [_QWERTY] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN,
-                                   QK_LEAD, LT(NAV, KC_TAB), KC_LSFT,   LALT_T(KC_SPC),  LT(SYM, KC_ESC),  KC_LCTL
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,  KC_J,    KC_K,    KC_L,    KC_QUOT,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                        KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SCLN,
+                                   QK_LEAD, NAV, KC_LSFT,                       KC_SPC,  SYM,   KC_LCTL
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
-        KC_EXLM,    KC_AT,    KC_HASH,    KC_DLR,    KC_PERC,                  KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_TILD,    KC_GRV,
-        KC_ESC,     KC_LCBR,  KC_LBRC,    KC_LPRN,   KC_MINS,                  KC_UNDS,    KC_RPRN,    KC_RBRC,    KC_RCBR,    KC_QUES,
+        KC_EXLM, KC_AT,    KC_HASH,    KC_DLR,    KC_PERC,                     KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_TILD,    KC_GRV,
+        KC_ESC,  KC_LCBR,  KC_LBRC,    KC_LPRN,   KC_MINS,                     KC_UNDS,    KC_RPRN,    KC_RBRC,    KC_RCBR,    KC_QUES,
         KC_NO,   KC_NO,    KC_PIPE,    KC_SLSH,   KC_EQL,                      KC_PLUS,    KC_BSLS,    KC_NO,      KC_NO,      KC_SCLN,
                                        KC_TRNS,   NUM,    KC_LSFT,     KC_SPC, KC_TRNS,    KC_LCTL
     ),
